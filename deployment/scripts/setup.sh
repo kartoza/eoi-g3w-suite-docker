6c3cca11c98ae7f549f08660f4a8c4318d187a53
Zulfikar Akbar Muzakki <zakki@kartoza.com>
2024-12-09 11:44:49 +0700

    Add log to trigger image build for submoduling g3w-api

--- deployment/scripts/setup.sh
+++ deployment/scripts/setup.sh
@@ -15,6 +15,7 @@ pip3 install -r requirements_huey.txt
 git submodule add -f https://github.com/g3w-suite/g3w-admin-frontend.git  g3w-admin/frontend
 
 # Dam monitor
+echo "Adding dam_monitor and g3w_api"
 git submodule add -f https://github.com/kartoza/eoi-g3w-dam-monitor.git  g3w-admin/dam_monitor
 git submodule add -f https://github.com/kartoza/eoi-g3w-api.git  g3w-admin/g3w_api
 
